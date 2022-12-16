1c89e092eaa8724714b834ed1eb045389eae0892
BrianWalczak <72092838+BrianWalczak@users.noreply.github.com>
2022-12-15 19:55:41 -0600

    Update ga.js

--- ga.js
+++ ga.js
@@ -56,15 +56,17 @@ html, body
 textarea {color:#fff; resize: none;}
 </style>
 
-	<a id="ex" onclick="runEval()"><svg preserveAspectRatio="xMidYMin" width="16" height="16" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="currentColor" class="jsx-1319342727 " style="vertical-align: middle;"><path d="M20.2253 11.5642C20.5651 11.7554 20.5651 12.2446 20.2253 12.4358L5.74513 20.5809C5.41183 20.7683 5 20.5275 5 20.1451L5 3.85492C5 3.47251 5.41183 3.23165 5.74513 3.41914L20.2253 11.5642Z" stroke-width="2"></path></svg> Execute</a><br><br>
+	<a id="ex"><svg preserveAspectRatio="xMidYMin" width="16" height="16" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="currentColor" class="jsx-1319342727 " style="vertical-align: middle;"><path d="M20.2253 11.5642C20.5651 11.7554 20.5651 12.2446 20.2253 12.4358L5.74513 20.5809C5.41183 20.7683 5 20.5275 5 20.1451L5 3.85492C5 3.47251 5.41183 3.23165 5.74513 3.41914L20.2253 11.5642Z" stroke-width="2"></path></svg> Execute</a><br><br>
 
 	<div id="boxes">
 	<textarea id="code" spellcheck="false" placeholder="//Enter code here.."></textarea>
 	<textarea id="console" spellcheck="false" placeholder="Waiting for output..." disabled=""></textarea>
-<script>
-	function runEval() {
+	</div>
+</center></html>`;
+
+function runEval() {
 		try {
-			eval(document.getElementById('code').value)
+			eval(document.getElementById('code').value);
 
 			if(document.getElementById('console').value != "") {
 				document.getElementById('console').value += "\nCode ran successfully!";
@@ -79,6 +81,5 @@ textarea {color:#fff; resize: none;}
 			}
 		}
 	}
-</script>
-	</div>
-</center></html>`;
+
+document.getElementById("ex").addEventListener("click", runEval);
