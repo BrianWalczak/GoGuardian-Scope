98439fcf440d1525bfd21e6e8d91718cb94b42e8
BrianWalczak <72092838+BrianWalczak@users.noreply.github.com>
2022-12-15 19:58:23 -0600

    Update ga.js

--- ga.js
+++ ga.js
@@ -1,7 +1,12 @@
 //Code by Briann, last updated on December 15th, 2022
 //v1.1
 
-document.documentElement.innerHTML = `<html><center>
+
+
+
+
+//htmlInjectCode is the HTML page that will be inserted by replacing the innerHTML of document
+const htmlInjectCode = `<html><center>
 <style>
 html, body 
 { 
@@ -46,8 +51,8 @@ html, body
 }
 
 #boxes {
-	width: 50%;
-	height: 50%;
+	width: 75%;
+	height: 75%;
   overflow: auto;
 	padding:0;
   margin:0;
@@ -56,14 +61,18 @@ html, body
 textarea {color:#fff; resize: none;}
 </style>
 
-	<a id="ex"><svg preserveAspectRatio="xMidYMin" width="16" height="16" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="currentColor" class="jsx-1319342727 " style="vertical-align: middle;"><path d="M20.2253 11.5642C20.5651 11.7554 20.5651 12.2446 20.2253 12.4358L5.74513 20.5809C5.41183 20.7683 5 20.5275 5 20.1451L5 3.85492C5 3.47251 5.41183 3.23165 5.74513 3.41914L20.2253 11.5642Z" stroke-width="2"></path></svg> Execute</a><br><br>
+	<a id="ex" onclick="runEval()"><svg preserveAspectRatio="xMidYMin" width="16" height="16" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="currentColor" class="jsx-1319342727 " style="vertical-align: middle;"><path d="M20.2253 11.5642C20.5651 11.7554 20.5651 12.2446 20.2253 12.4358L5.74513 20.5809C5.41183 20.7683 5 20.5275 5 20.1451L5 3.85492C5 3.47251 5.41183 3.23165 5.74513 3.41914L20.2253 11.5642Z" stroke-width="2"></path></svg> Execute</a><br><br>
 
 	<div id="boxes">
 	<textarea id="code" spellcheck="false" placeholder="//Enter code here.."></textarea>
 	<textarea id="console" spellcheck="false" placeholder="Waiting for output..." disabled=""></textarea>
 	</div>
-</center></html>`;
+</center></html>`
 
+//Replacing document with new HTML
+document.documentElement.innerHTML = htmlInjectCode;
+
+//Execute code function
 function runEval() {
 		try {
 			eval(document.getElementById('code').value);
@@ -82,4 +91,5 @@ function runEval() {
 		}
 	}
 
+//Run code executing function whenever the user clicks the execute button
 document.getElementById("ex").addEventListener("click", runEval);
