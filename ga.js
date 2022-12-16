7a44ce93bfb3efc2614cd7db38a7d3f36997afc0
BrianWalczak <72092838+BrianWalczak@users.noreply.github.com>
2022-12-16 16:58:13 -0600

    Update ga.js

--- ga.js
+++ ga.js
@@ -10,10 +10,8 @@ const htmlInjectCode = `<html><center>
 <style>
 html, body {
 	margin-top: 20px;
-	overflow: hidden;
 	background-color: #41A22D;
 }
-
 #ex {
 	color: #1d6e15;
 	text-align: center;
@@ -29,12 +27,10 @@ html, body {
 	transition: 0.3s;
 	font-family: arial;
 }
-
 #ex:hover {
 	color: white;
 	background-color: #2EA11B;
 }
-
 #code {
 	width: 60%;
 	height: 100%;
@@ -44,7 +40,6 @@ html, body {
 	border: 3px solid white;
 	border-right: 1.5px solid white;
 }
-
 #console {
 	width: 40%;
 	height: 100%;
@@ -54,7 +49,6 @@ html, body {
 	border: 3px solid white;
 	border-left: 1.5px solid white;
 }
-
 #boxes {
 	width: 75%;
 	height: 75%;
@@ -62,21 +56,27 @@ html, body {
 	padding:0;
   margin:0;
 }
-
 textarea {color:#fff; resize: none;}
 h1, h3, p {font-family: sans-serif; color: #fff;}
 </style>
-	<b><h1 style="margin:0; padding:0;">Scope (for GoGuardian)</h1></b>
+	<h1 style="margin:0; padding:0;">Scope (for GoGuardian)</h1>
 	<h3 style="margin:0; padding:0;">Have any issues? Contact me on Discord at brian.#8330</h3>
 	<br><br>
 	<a id="ex" onclick="runEval()"><svg preserveAspectRatio="xMidYMin" width="16" height="16" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="currentColor" class="jsx-1319342727 " style="vertical-align: middle;"><path d="M20.2253 11.5642C20.5651 11.7554 20.5651 12.2446 20.2253 12.4358L5.74513 20.5809C5.41183 20.7683 5 20.5275 5 20.1451L5 3.85492C5 3.47251 5.41183 3.23165 5.74513 3.41914L20.2253 11.5642Z" stroke-width="2"></path></svg> Execute</a><br><br>
-
 	<div id="boxes">
 	<textarea id="code" spellcheck="false" placeholder="//Enter code here.."></textarea>
 	<textarea id="console" spellcheck="false" placeholder="Waiting for output..." disabled=""></textarea>
 	</div>
+	<br><br><hr><br><br>
 
-<br>
+	<h1 style="margin:0; padding:0;">Useful Scripts</h1>
+	<h3 style="margin:0; padding:0;">Select a script from the dropdown below to use it!</h3>
+	<br>
+	<select id="scriptSelect">
+		<option value="0">Select a script...</option>
+		<option value="1">Enable any Chrome extension</option>
+		<option value="2">Disable any Chrome extension</option>
+	</select>
 </center></html>`
 
 //Replacing document with new HTML
