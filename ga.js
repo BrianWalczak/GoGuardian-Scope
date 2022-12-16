08edeece0dcffeb842f56030022cbb3602bc3871
BrianWalczak <72092838+BrianWalczak@users.noreply.github.com>
2022-12-15 19:51:07 -0600

    Update ga.js

--- ga.js
+++ ga.js
@@ -1,4 +1,84 @@
 //Code by Briann, last updated on December 15th, 2022
 //v1.1
 
-//not done
+document.documentElement.innerHTML = `<html><center>
+<style>
+html, body 
+{ 
+ overflow: hidden
+}
+
+#ex {
+	color: #1d6e15;
+	text-align: center;
+	text-decoration: none;
+	display: inline-block;
+	font-size: 16px;
+	cursor: pointer;
+	font-weight: bold;
+	border-radius: 10px;
+	background: #7AEB8D;
+	padding: 10px 13px;
+	border: 0.5px solid #7AEB8D;
+	transition: 0.3s;
+	font-family: arial;
+}
+
+#ex:hover {
+	color: white;
+	background-color: #2EA11B;
+}
+
+#code {
+	width: 60%;
+	height: 100%;
+	padding: 20px;
+	float: left;
+	background:#34495E;
+}
+
+#console {
+	width: 40%;
+	height: 100%;
+	padding: 20px;
+	float: left;
+	background:#000;
+}
+
+#boxes {
+	width: 50%;
+	height: 50%;
+  overflow: auto;
+	padding:0;
+  margin:0;
+}
+
+textarea {color:#fff; resize: none;}
+</style>
+
+	<a id="ex" onclick="runEval()"><svg preserveAspectRatio="xMidYMin" width="16" height="16" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="currentColor" class="jsx-1319342727 " style="vertical-align: middle;"><path d="M20.2253 11.5642C20.5651 11.7554 20.5651 12.2446 20.2253 12.4358L5.74513 20.5809C5.41183 20.7683 5 20.5275 5 20.1451L5 3.85492C5 3.47251 5.41183 3.23165 5.74513 3.41914L20.2253 11.5642Z" stroke-width="2"></path></svg> Execute</a><br><br>
+
+	<div id="boxes">
+	<textarea id="code" spellcheck="false" placeholder="//Enter code here.."></textarea>
+	<textarea id="console" spellcheck="false" placeholder="Waiting for output..." disabled=""></textarea>
+<script>
+	function runEval() {
+		try {
+			eval(document.getElementById('code').value)
+
+			if(document.getElementById('console').value != "") {
+				document.getElementById('console').value += "\nCode ran successfully!";
+			}else{
+				document.getElementById('console').value += "Code ran successfully!";
+			}
+		} catch (e) {
+			if(document.getElementById('console').value != "") {
+				document.getElementById('console').value += "\nError: " + e.message;
+			}else{
+				document.getElementById('console').value += "Error: " + e.message;
+			}
+		}
+	}
+</script>
+	</div>
+</center></html>`;
